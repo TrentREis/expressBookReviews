1ba7ad3dcc365cf81f92f633bc51d9dfc5f7dad3
TrentREis <133795091+TrentREis@users.noreply.github.com>
2025-03-23 14:48:10 -0400

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -48,7 +48,19 @@ public_users.get('/', async function (req, res) {
       res.status(404).json({ message: "Book not found" });
     }
   });
+
+
+  public_users.get('/booksdb/author/:author', function (req, res) {
+    const author = req.params.author;
+    const matchingBooks = Object.values(books).filter(book => book.author === author);
   
+    if (matchingBooks.length > 0) {
+      res.status(200).json(matchingBooks);
+    } else {
+      res.status(404).json({ message: "No books found for the given author" });
+    }
+  });
+
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', async function (req, res) {
     const isbn = req.params.isbn;
@@ -62,20 +74,14 @@ public_users.get('/isbn/:isbn', async function (req, res) {
   });
   
 // Get book details based on author
-public_users.get('/author/:author', function (req, res) {
+public_users.get('/author/:author', async function (req, res) {
     const author = req.params.author;
-    const matchingBooks = [];
   
-    Object.keys(books).forEach((isbn) => {
-      if (books[isbn].author === author) {
-        matchingBooks.push(books[isbn]);
-      }
-    });
-  
-    if (matchingBooks.length > 0) {
-      res.status(200).json(matchingBooks);
-    } else {
-      res.status(404).json({ message: "No books found for the given author" });
+    try {
+      const response = await axios.get(`http://localhost:5000/booksdb/author/${author}`);
+      res.status(200).json(response.data);
+    } catch (error) {
+      res.status(404).json({ message: "No books found", error: error.message });
     }
   });
   
