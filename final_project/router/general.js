19f1b83313040a0011b34de3fc3a5b49ab6d6995
TrentREis <133795091+TrentREis@users.noreply.github.com>
2025-03-23 14:41:07 -0400

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,6 +3,7 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require('axios');
 
 
 public_users.post("/register", (req, res) => {
@@ -25,11 +26,17 @@ public_users.post("/register", (req, res) => {
   
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-     res.send(JSON.stringify(books,null,10));
- // return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.get('/', async function (req, res) {
+    try {
+      const response = await axios.get('http://localhost:5000/booksdb');
+      res.status(200).json(response.data);
+    } catch (error) {
+      res.status(500).json({ message: "Error fetching books", error: error.message });
+    }
+  });
+  public_users.get('/booksdb', function (req, res) {
+    res.status(200).json(books);
+  });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
