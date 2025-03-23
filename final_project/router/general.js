92ea54dc529065d97cfbde253fcb670d3ac8388c
TrentREis <133795091+TrentREis@users.noreply.github.com>
2025-03-23 13:25:24 -0400

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,12 +18,17 @@ public_users.get('/',function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  const isbn = req.params.isbn;
-  res.send(books[isbn]);
- // return res.status(300).json({message: "Yet to be implemented"});
- });
+// Get book details based on ISBN
+public_users.get('/isbn/:isbn', function (req, res) {
+    const isbn = req.params.isbn;
+    const book = books[isbn];
+  
+    if (book) {
+      res.status(200).json(book);
+    } else {
+      res.status(404).json({ message: "Book not found for the given ISBN" });
+    }
+  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
